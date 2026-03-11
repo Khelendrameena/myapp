9158fabdf3b5ad0fb3c812627ee57516118f656e
Khelendra meena <168931780+Khelendrameena@users.noreply.github.com>
2026-03-11 18:33:00 +0530

    Include external script in page component
    Added a script tag for an external JavaScript file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import Script from "next/script";
 import ChatRoom from '@/components/ChatRoom'
 import MatchingScreen from '@/components/MatchingScreen'
 import HomePage from '@/components/HomePage'
@@ -120,6 +121,10 @@ export default function Home() {
 
   return (
     <main className="min-h-screen flex flex-col">
+      <Script
+  src="https://pl28894878.effectivegatecpm.com/8b/5b/6b/8b5b6bee367f1ec626175a4d6d0d3e56.js"
+  strategy="afterInteractive"
+/>
       {!matched ? (
         <MatchingScreen 
           onStartMatching={startMatching}
